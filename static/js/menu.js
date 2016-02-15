3976e2d02500ec63639a5e4f03024c831e387bde
nbeclin <nbeclin@amg-dev.fr>
2016-02-15 13:27:43 +0100

    Ajout du script pour le menu

---
+++ static/js/menu.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function($){	
+
+	$(".ss-bt").hide();
+
+	$(".1-1").click(function ()
+	{
+		$(".ss-bt").hide();
+		$(".1-2").show(); 
+	})
+	
+	$(".2-1").click(function ()
+	{
+		$(".ss-bt").hide(); 
+		$(".2-2").show(); 
+	})
+	
+	$(".3-1").click(function ()
+	{
+		$(".ss-bt").hide(); 
+		$(".3-2").show(); 
+	})
+	
+	$(".4-1").click(function ()
+	{
+		$(".ss-bt").hide(); 
+		$(".4-2").show(); 
+	})
+	
+	$(".5-1").click(function ()
+	{
+		$(".ss-bt").hide(); 
+		$(".5-2").show(); 
+	})
+	
+	$(".6-1").click(function ()
+	{
+		$(".ss-bt").hide(); 
+		$(".6-2").show(); 
+	})
+	
+	$(".7-1").click(function ()
+	{
+		$(".ss-bt").hide(); 
+		$(".7-2").show(); 
+	})
+
+});
