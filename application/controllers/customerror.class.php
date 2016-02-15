1afcd9af0610fb1c92b16c92804573c3d9ffff3c
nbeclin <nbeclin@amg-dev.fr>
2016-02-15 14:01:34 +0100

    Ajout du style à l'erreur 404

--- application/controllers/customerror.class.php
+++ application/controllers/customerror.class.php
@@ -8,6 +8,7 @@ class CustomError extends Controller {
     function error404(){
         $template = $this->loadView('error404');
         $template->set('static', $this->staticFiles);
+        $template->addCss('style', 'css');
         $template->set('title', 'ERREUR 404');
         $template->set('nav', null);
         $template->render();
