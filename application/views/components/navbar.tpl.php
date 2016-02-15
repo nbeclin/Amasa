694af974e1ce1c15fdf8f3ffa5fb07be59ffe895
nbeclin <nbeclin@amg-dev.fr>
2016-02-15 13:32:04 +0100

    Modification menu (s à page)

--- application/views/components/navbar.tpl.php
+++ application/views/components/navbar.tpl.php
@@ -1,21 +1,21 @@
 <ul class="nav nav-pills nav-stacked">
 	<li class="active 1-1"><a href="#" class=""><span class="gras">AMASA</span></a></li>
-		<li class="ss-bt 1-2"><a href="/<?php echo BASE_URL ?>page/association">L'association</a></li>
-		<li class="ss-bt 1-2"><a href="/<?php echo BASE_URL ?>page/bureau">Le bureau</a></li>
+		<li class="ss-bt 1-2"><a href="/<?php echo BASE_URL ?>pages/association">L'association</a></li>
+		<li class="ss-bt 1-2"><a href="/<?php echo BASE_URL ?>pages/bureau">Le bureau</a></li>
 	<li class="active 2-1"><a href="/<?php echo BASE_URL ?>condAdoption"><span class="gras">Conditions d'adoption</span></a></li>
 	<li class="active 3-1"><a href="#"><span class="gras">Adoptions</span></a></li>
-		<li class="ss-bt 3-2"><a href="/<?php echo BASE_URL ?>page/adoption/chat">Chats adultes</a></li>
-		<li class="ss-bt 3-2"><a href="/<?php echo BASE_URL ?>page/adoption/chaton">Chatons</a></li>
-		<li class="ss-bt 3-2"><a href="/<?php echo BASE_URL ?>page/adoption/chien">Chiens</a></li>
+		<li class="ss-bt 3-2"><a href="/<?php echo BASE_URL ?>pages/adoption/chat">Chats adultes</a></li>
+		<li class="ss-bt 3-2"><a href="/<?php echo BASE_URL ?>pages/adoption/chaton">Chatons</a></li>
+		<li class="ss-bt 3-2"><a href="/<?php echo BASE_URL ?>pages/adoption/chien">Chiens</a></li>
 	<li class="active 4-1"><a href="#"><span class="gras">Les adoptés</span></a></li>
-		<li class="ss-bt 4-2"><a href="/<?php echo BASE_URL ?>page/adopte/2015">2015</a></li>
-		<li class="ss-bt 4-2"><a href="/<?php echo BASE_URL ?>page/adopte/2014">2014</a></li>
-		<li class="ss-bt 4-2"><a href="/<?php echo BASE_URL ?>page/adopte/2013">2013</a></li>
+		<li class="ss-bt 4-2"><a href="/<?php echo BASE_URL ?>pages/adopte/2015">2015</a></li>
+		<li class="ss-bt 4-2"><a href="/<?php echo BASE_URL ?>pages/adopte/2014">2014</a></li>
+		<li class="ss-bt 4-2"><a href="/<?php echo BASE_URL ?>pages/adopte/2013">2013</a></li>
 	<li class="active 5-1"><a href="#"><span class="gras">Nous aider</span></a></li>
-		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>page/devFamille">Devenir Famille d'accueil</a></li>
-		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>page/adherer">Adhérer à l'association</a></li>
-		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>page/don">Faire un don</a></li>
-		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>page/parrainer">Parrainer un animal</a></li>
+		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>pages/devFamille">Devenir Famille d'accueil</a></li>
+		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>pages/adherer">Adhérer à l'association</a></li>
+		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>pages/don">Faire un don</a></li>
+		<li class="ss-bt 5-2"><a href="/<?php echo BASE_URL ?>pages/parrainer">Parrainer un animal</a></li>
 	<li class="active 6-1"><a href="#"><span class="gras">Liens utiles</span></a></li>
 <li class="active 7-1"><a href="#"><span class="gras">Revus de presse</span></a></li>
 </ul>
