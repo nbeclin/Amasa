d7b28bc3d770a495cc7556e830bd83ba262d6047
Nicolas BECLIN <nbeclin@amg-dev.fr>
2016-02-16 21:38:41 +0100

    Début de mise en page -ADOPTE-

--- application/views/front/pages/adopte.php
+++ application/views/front/pages/adopte.php
@@ -5,7 +5,14 @@
 		<?php if ($cpt % 3 == 0) : ?>
 			<div class="row">
 		<?php endif; ?>
-			
+			<div class="col-md-4 img-portfolio">
+				<a href="/<?php echo BASE_URL ?>img/animaux/<?php echo $animal->photos[0]->lien ?>" data-toggle="lightbox" data-gallery="multiimages" data-title="<h3><span class=<?php echo ($animal->sexe == 2) ? 'rose' : 'bleu' ?>><?php echo $animal->nom ?></span></h3>">	
+					<img class="img-responsive img-hover" src="/<?php echo BASE_URL ?>img/animaux/00-<?php echo $animal->photos[0]->lien ?>" alt="amasa">
+				</a>
+				<h3>
+					<span class ="<?php echo ($animal->sexe == 1) ? 'bleu' : 'rose' ?> gras"><?php echo $animal->nom ?></span>
+				</h3>	
+			</div>
 
 		<?php if ($cpt % 3 == 2) : ?>
 			</div>
@@ -14,33 +21,7 @@
 	<?php endforeach; ?>
 
 
-<?php while($data = mysql_fetch_assoc($req)) : ?>
-		<?php if ($cpt % 3 == 0) : ?>
-		<div class="row">
-		<?php endif; ?>
-			<?php 
-				if ($data['sexe'] == 2) {
-					$couleur = '<h3><span class=rose>'.$data['nom'].'</span></h3>';
-				}
-				else {
-					$couleur = '<h3><span class=bleu>'.$data['nom'].'</span></h3>';
-				}
-			?>
-			<div class="col-md-4 img-portfolio">
-				<!--<a href="portfolioAdopte.php?page=adopte&amp;annee=<?php //echo $annee ?>&amp;num_page=<?php //echo $num_page ?>&amp;idAnimal=<?php //echo $data['id'] ?>">-->
-				<a href="administration/application/img/animaux/<?php echo $data['lien'] ?>" data-toggle="lightbox" data-gallery="multiimages" data-title="<?php echo $couleur; ?>">	
-					<img class="img-responsive img-hover" src="administration/application/img/animaux/00-<?php echo $data['lien'] ?>" alt="amasa">
-				</a>
-				<h3>
-					<span class ="<?php echo ($data['sexe'] == 1) ? 'bleu' : 'rose' ?> gras"><?php echo $data['nom'] ?></span>
-				</h3>	
-			</div>
-		<?php if ($cpt % 3 == 2) : ?>
-		</div>
-		<?php endif; ?>
-		
-		<?php $cpt++; ?>
-<?php endwhile; ?>
+
 <?php 
 	switch (($cpt) % 3) {
 		case 0 :
@@ -62,4 +43,4 @@
 			';
 			break;
 	}
-<?php var_dump($animaux) ?>
+?>
