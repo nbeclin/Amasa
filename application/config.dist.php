06ecb6288d4d81d0c08adc233bd7961bdc594b58
Nicolas BECLIN <nbeclin@amg-dev.fr>
2016-02-16 21:45:18 +0100

    Ajout config.dist

---
+++ application/config.dist.php
@@ -0,0 +1,18 @@
+<?php
+$config['base_url'] = 'AMASA/';
+$config['project'] = 'AMASA';
+$config['admins'] = array();
+$config['default_controller'] = 'main';
+$config['default_admin_controller'] = 'main';
+$config['error_controller'] = 'customerror';
+$config['admin_mode'] = 'admin';
+$config['base_template'] = 'base';
+$config['base_template_admin'] = 'admin';
+$config['db_host'] = '';
+$config['db_name'] = '';
+$config['db_username'] = '';
+$config['db_password'] = '';
+$config['autoload'] = array(
+    'plugins' => array(),
+    'helpers' => array('session', 'url', 'staticFiles')
+);
