4223eefb690db6ef5c0954959a575f04fed3873d
Nicolas BECLIN <nbeclin@amg-dev.fr>
2016-02-20 11:52:15 +0100

    Mise en forme du code

--- static/js/banner.js
+++ static/js/banner.js
@@ -1,16 +1,16 @@
 function slide(cpt){
-			for (i=1 ; i<31;i++){
-				if(eval(cpt)+i <= 30){
-					document.getElementById('img' + i).src = 'img/bandeau/img' + (eval(cpt)+i) + '.jpg'; 
-				}
-				else {
-					document.getElementById('img' + i).src = 'img/bandeau/img' + (eval(cpt)+i-30) + '.jpg'; 
-				}
-			}
-			if(eval(cpt)<30){
-				document.getElementById('cpt').value = eval(cpt) + 1;
-			}
-			else {
-				document.getElementById('cpt').value = 1;
-			}
+	for (i=1 ; i<31;i++){
+		if(cpt.val()+i <= 30){
+			document.getElementById('img' + i).src = 'img/bandeau/img' + (eval(cpt)+i) + '.jpg'; 
 		}
+		else {
+			document.getElementById('img' + i).src = 'img/bandeau/img' + (eval(cpt)+i-30) + '.jpg'; 
+		}
+	}
+	if(eval(cpt)<30){
+		document.getElementById('cpt').value = eval(cpt) + 1;
+	}
+	else {
+		document.getElementById('cpt').value = 1;
+	}
+}
