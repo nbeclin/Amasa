e4b81fbc60faea445051d6bd7ce028a7f839eb13
Nicolas BECLIN <nbeclin@amg-dev.fr>
2016-02-21 18:26:19 +0100

    Encadrer pour page d'acueil

--- application/views/front/pages/commun.php
+++ application/views/front/pages/commun.php
@@ -1,4 +1,10 @@
 <div id="contenu">
 	<h1><?php echo $info_page->titre ?></h1>
-	<?php echo $info_page->texte ?>
+	<?php if ($info_page->titre == 'Accueil') :?>
+		<div class="pres_accueil">
+			<?php echo $info_page->texte ?>
+		</div>		
+	<?php else : ?>
+		<?php echo $info_page->texte ?>
+	<?php endif; ?>		
 </div>
