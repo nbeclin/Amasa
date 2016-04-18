5e4a749034554de2ae4c2198fa9eab17efcaa967
nbeclin <nbeclin@amg-dev.fr>
2016-04-18 12:52:45 +0200

    Modification menu

--- application/views/components/admin/navbar-admin.tpl.php
+++ application/views/components/admin/navbar-admin.tpl.php
@@ -20,7 +20,7 @@
                     <li class="">
                         <a href="/<?php echo BASE_URL; ?>" target="_blank"><span class="glyphicon glyphicon-eye-open" style="color:#81DAF5;"></span></a>
                     </li>
-                    <li class="<?php echo $nav == 'pages' ? 'active' : ''; ?> li-separation">
+                    <li class="<?php echo $nav == 'pages' ? 'active' : ''; ?>">
                         <a href="/<?php echo BASE_URL; ?>admin/pages">Pages</a>
                     </li>
                     <li class="<?php echo $nav == 'animaux' ? 'active' : ''; ?>">
@@ -29,10 +29,10 @@
                     <li class="<?php echo $nav == 'photos' ? 'active' : ''; ?>">
                         <a href="/<?php echo BASE_URL; ?>admin/photos">Photos</a>
                     </li>
-                    <li class="<?php echo $nav == 'stats' ? 'active' : ''; ?> li-separation">
+                    <li class="<?php echo $nav == 'stats' ? 'active' : ''; ?>">
                         <a href="/<?php echo BASE_URL; ?>admin/stats"><span class="glyphicon glyphicon-stats"></span></a>
                     </li>
-                    <li class="<?php echo $nav == 'logout' ? 'active' : ''; ?> li-separation">
+                    <li class="<?php echo $nav == 'logout' ? 'active' : ''; ?>">
                         <a href="/<?php echo BASE_URL; ?>admin/logout"><span class="glyphicon glyphicon-user" style="color:#088A29;"></span> <span style="color:#088A29;"><?php echo $user->login ?></span> <span class="glyphicon glyphicon-log-out"></span></a>
                     </li>
                 <?php endif; ?>
