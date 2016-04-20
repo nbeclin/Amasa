04c7999326f8a6bf1c16b25922c2c3d35fc03914
nbeclin <nbeclin@amg-dev.fr>
2016-04-20 13:23:33 +0200

    Optimisation des données statistiques

--- application/models/stat_all.class.php
+++ application/models/stat_all.class.php
@@ -1,17 +1,38 @@
 <?php
 class Stat_all extends Model {
-
-    public $all = array();
+    
 
     public function __construct(){
         parent::__construct();
     }
 
     public function load_all(){
+        $results = array();
+
+        $date = null;
+        $nb_visitor = 0;
+        $nb_page = 0;
+
         foreach($this->selectAll('stat', '*', null, 'ORDER BY date DESC') as $stat){
-            array_push($this->all, new Stat($stat));
+            if ($stat['date'] == $date) {
+                $nb_visitor++;
+                $nb_page = $nb_page + $stat['nb_page'];
+            }
+            else {
+                if ($date != null){
+                    array_push($results, array( 'date' => $this->date_conversion_fr($date),
+                                            'nb_visitor' => $nb_visitor,
+                                            'nb_page' => $nb_page,
+                                            'average_page' => round($nb_page/$nb_visitor, 2, PHP_ROUND_HALF_UP)
+                                    ));
+                }
+                $nb_visitor = 1;
+                $nb_page = $stat['nb_page'];
+                $date = $stat['date'];
+            }
         }
-        return $this->all;
+
+        return $results;
     }
 
     public function register($ip){
@@ -27,4 +48,16 @@ class Stat_all extends Model {
             $this->update('stat', array('nb_page' => $results['0']->nb_page + 1), array('ip' => $ip, 'date' => date('Y-m-d')));
         }    
     }
+
+
+    /** 
+    * Function date_conversion_fr
+    * @param $dateToConvert - datetime format
+    * @return string - fr date format 
+    */
+    private function date_conversion_fr($dateToConvert){
+        $date = new DateTime($dateToConvert);
+            return $date->format('d/m/Y');
+    }
+
 }
--- application/views/admin/stats/main.php
+++ application/views/admin/stats/main.php
@@ -3,21 +3,25 @@
     <thead>
         <tr>
             <th class="text-center">Date</th>
-            <th class="text-center">IP</th>
+            <th class="text-center">Nombre de visiteur</th>
             <th class="text-center">Nombre de page</th>
+            <th class="text-center">Moyenne par visiteur</th>            
         </tr>
     </thead>
     <tbody>
 		<?php foreach ($stats as $stat) : ?>	
 			<tr>
-				<td class="text-left">
-					<?php echo $stat->date ?>
+				<td class="text-center">
+					<?php echo $stat['date'] ?>
 				</td>
 				<td class="text-center">
-					<?php echo $stat->ip ?>
+					<?php echo $stat['nb_visitor'] ?>
 				</td>
 				<td class="text-center">
-					<?php echo $stat->nb_page ?>
+					<?php echo $stat['nb_page'] ?>
+				</td>
+				<td class="text-center">
+					<?php echo $stat['average_page'] ?>
 				</td>
 			</tr>
 		<?php endforeach; ?>
